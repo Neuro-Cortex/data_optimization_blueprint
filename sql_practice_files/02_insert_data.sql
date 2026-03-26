22d7fec04170cf92e014373aeb54a491dce5bceb
Pollob <104382051+Neuro-Cortex@users.noreply.github.com>
2026-03-26 23:57:13 +0600

    Add files via upload

---
+++ sql_practice_files/02_insert_data.sql
@@ -0,0 +1,13 @@
+INSERT INTO Students VALUES
+(1, 'Rahim', 'CSE', 22),
+(2, 'Karim', 'EEE', 23),
+(3, 'Sana', 'CSE', 21);
+
+INSERT INTO Courses VALUES
+(101, 'Database', 'Mr. A'),
+(102, 'Algorithms', 'Mr. B');
+
+INSERT INTO Enrollments VALUES
+(1, 1, 101),
+(2, 2, 102),
+(3, 1, 102);
